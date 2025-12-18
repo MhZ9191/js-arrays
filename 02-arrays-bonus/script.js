9cf04988fea213acbf140a3f204be2903f7429b5
MhZ9191 <hegic91@gmail.com>
2025-12-18 15:59:20 +0100

    Added Bonus

--- 02-arrays-bonus/script.js
+++ 02-arrays-bonus/script.js
@@ -13,13 +13,34 @@ console.table(reversedTeachers);
 
 // 2. Crea un nuovo array chiamato 'longNames' che contenga solo gli insegnanti
 // con un nome di lunghezza maggiore o uguale a 5 caratteri
-const longNames = null;
+const longNames = [];
+
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i].length >= 5) {
+    longNames.push(teachers[i]);
+  }
+}
+console.table(longNames);
 
 // 3. Rimuovi 'Ed' dall'array teachers
 
+teachers.splice(1, 1);
+console.table(teachers);
+
 // 4. Verifica se 'Fabio' è presente nell'array teachers
 // e salva il risultato nella variabile isFabioPresent
-const isFabioPresent = null;
+let isFabioPresent = null;
+for (let i = 0; i < teachers.length; i++) {
+  if (teachers[i] === "Fabio") {
+    isFabioPresent = true;
+  }
+}
+console.log(isFabioPresent);
 
 // 5. Unisci tutti gli insegnanti nell'array teachers in una stringa  separata da virgole e salvala nella variabile teachersString
-const teachersString = null;
+let teachersString = "";
+
+for (let i = 0; i < teachers.length; i++) {
+  teachersString += teachers[i] + ",";
+}
+console.log(teachersString);
